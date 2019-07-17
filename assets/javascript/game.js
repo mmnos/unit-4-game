5693b47c9d1cc714460ab19da1d815b2558e6bcf
Michael N <mnos9897@gmail.com>
2019-07-17 18:57:44 -0400

    Rough draft of HTML complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// gets a random number between 19-120
+let randNum = Math.floor(Math.random() * 102) + 19;
+// gets a random number between 1-12
+let imgRandNum = Math.floor(Math.random() * 12) + 1;
+console.log(randNum);
